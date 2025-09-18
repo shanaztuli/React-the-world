a7741a1d8ebbdfc88110ab0a10b06418314cf593
shanaz tuli <shanazparbinloskortuli@gmail.com>
2025-09-18 20:17:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Countries from "./Components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
